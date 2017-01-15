9d5f1e4184f70ab930ebe17968dc8c748f3018c4
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2017-01-16 00:46:41 +0100

    Created template for future tmux scripts

---
+++ start-tmux.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+export TmuxSessionName='TestSession'
+source "${0:a:h}/.tmux-helpers.sh"
+
+function initiate-tmux() {
+  # Initiate with TmuxSessionName
+  new-session;
+
+  # Initiate window 0
+  Window0=$TmuxSessionName:0
+  splitwh-setup $Window0 '.' \
+      'ps aux' \
+      'ls -la' \
+  ;
+  setup-window $Window0;
+  rename-window $Window0 'Window 0';
+
+  # Initiate window 1
+  Window1=$TmuxSessionName:1
+  new-window-splitwh-setup $Window1 '.' 'Window 1' \
+      'echo left' \
+      'echo right' \
+  ;
+
+  select-window $Window0;
+}
+
+if ! is-initiated; then
+  initiate-tmux
+fi
+
+attach-tmux
